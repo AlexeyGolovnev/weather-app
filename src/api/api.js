9dfd48cd1a42c3df2b8dd0f6f8412fd37d768443
Alexey Golovnev <golovnevalexey@gmail.com>
2020-11-10 17:45:58 +0300

    tiny modification

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
+const API_KEY = '6a416e2949384bc0aba103814200911';
 const instance = axios.create({
-    baseURL: '',
+    baseURL: 'http://api.weatherapi.com/v1',
 })
+const date = new Date();
+
+export function getCurrentWeather(city) {
+    instance.get(`current.json?${API_KEY}&q=${city}`);
+}
+
+export function getForecastWeather(city) {
+    instance.get(`forecast.json?${API_KEY}&q=${city}&days=7`);
+}
+
+export function getAstronomyWeather(city) {
+    instance.get(`astronomy.json?${API_KEY}&q=${city}&dt=${date.getFullYear()}-${date.getMonth()}-${date.getDate}`);
+}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Container, Title, SubTitle, Text } from './../../globalStyles'
 import { 
     MainSection, 
@@ -12,7 +12,6 @@ import {
     MainIconBox,
     MainIcon,
     MainTemperatureBox,
-    MainTextBox,
 }
 from "./Main.elements";
 
