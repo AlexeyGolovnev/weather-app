9f7342c4eb28155a0aa833c48c1374e924e5398f
Alexey Golovnev <golovnevalexey@gmail.com>
2020-11-18 13:53:50 +0300

    fix gitignore, api

--- src/api/api.js
+++ src/api/api.js
@@ -5,19 +5,15 @@ const instance = axios.create({
     baseURL: 'http://api.weatherapi.com/v1',
 })
 const date = new Date();
-
 export function getCurrentWeatherFromApi(city) {
     return instance.get(`current.json?key=${API_KEY}&q=${city}`);
 }
-
 export function getForecastWeatherFromApi(city) {
     return instance.get(`forecast.json?key=${API_KEY}&q=${city}&days=7`);
 }
-
 export function getAstronomyWeatherFromApi(city) {
     return instance.get(`astronomy.json?key=${API_KEY}&q=${city}&dt=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
 }
-
 export function getCitiesListFromApi(value) {
     return instance.get(`search.json?key=${API_KEY}&q=${value}`);
 }
